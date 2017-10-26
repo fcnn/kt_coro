62b6309423bd439defa70dd449d37e05e60f2134
unknown <a@a.cc>
2017-10-26 21:36:54 +0800

    .

--- src/main/kotlin/hello/Http.kt
+++ src/main/kotlin/hello/Http.kt
@@ -1,28 +1,21 @@
 package hello
 
+import com.google.protobuf.Message
 import java.io.IOException
-import kotlinx.coroutines.experimental.*
 import okhttp3.Call
 import okhttp3.Callback
-import okhttp3.Headers
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import okhttp3.Response
 import okhttp3.RequestBody
-import okhttp3.ResponseBody
 import okhttp3.MediaType
 
 import protofiles.protojava.CommonProtos as CommonProto
-import protofiles.protojava.MessagingProto
-import protofiles.protojava.UserProto
 import protofiles.protojava.CodeInProtos as CodeInProto
 
-class Request {
-    var name = "Request"
-}
-
-class Reply {
-    var name = "Reply"
+interface GwCallback {
+    fun onError()
+    fun onReply(reply: com.google.protobuf.Message)
 }
 
 class Http {
@@ -75,7 +68,6 @@ class Http {
                 .build()
 
         val call = client.newCall(request)
-        //call.enqueue(object : Callback {
         call.enqueue(object: Callback {
             override fun onFailure(call: Call, e: IOException) {
                 e.printStackTrace()
@@ -103,25 +95,111 @@ class Http {
 
     }
 
-    fun post(name: String, req: com.google.protobuf.Message, rep: com.google.protobuf.Message.Builder) {
-        var ba = ByteArray(10)
+    fun gwCall(api: String, req: com.google.protobuf.Message, builder: com.google.protobuf.Message.Builder, callback: GwCallback?) {
+        val mtype = MediaType.parse("application/octet-stream")
+        val body = RequestBody.create(mtype, req.toByteArray())
+        val request = Request.Builder()
+                .url(gwUrl + api)
+                .post(body)
+                .build()
+
+        val call = client.newCall(request)
+        call.enqueue(object: Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                e.printStackTrace()
+                callback?.onError()
+            }
+
+            @Throws(IOException::class)
+            override fun onResponse(call: Call, response: Response) {
+                response.body()!!.use { responseBody ->
+                    if (!response.isSuccessful) throw IOException("Unexpected code " + response)
+
+                    var reply = builder.mergeFrom(responseBody.byteStream()).build()
+                    callback?.onReply(reply)
+                }
+            }
+        })
+    }
+
+    fun post(req: com.google.protobuf.Message, rep: com.google.protobuf.Message.Builder) {
+        val reqStr = req.toString()
+        println("request: $reqStr")
+        var b = CodeInProto.LoginReply.newBuilder()
+        b.liveTopic = "topic/live"
+        b.userInfoBuilder.uid = 2017002L
+        b.userInfoBuilder.name = "青云"
+        var msg = b.build()
+
+        var ba = msg.toByteArray()
         rep.mergeFrom(ba)
     }
 
     companion object {
 
-        @Throws(Exception::class)
-        @JvmStatic
-        fun main(args: Array<String>) {
-            var http = Http();
+        private fun testGw(testId: Int) {
+            val req = CodeInProto.GetMwRequest.newBuilder()
+            req.gameId = 0
+            val clinfo = req.clientInfoBuilder
+            clinfo.uid = 2017009L
+            clinfo.token = "kt_coro_http"
+            clinfo.deviceId = "kt_coro_dev_000"
+            val reply = CodeInProto.GetMwReply.newBuilder()
+            val start = System.currentTimeMillis()
+            Http().gwCall("GetMw", req.build(), reply, object: GwCallback {
+                override fun onReply(reply: Message) {
+                    if (reply is CodeInProto.GetMwReply) {
+                        val timeCost = System.currentTimeMillis() - start
+                        (0 until reply.listCount)
+                                .map { reply.getList(it) }
+                                .forEach { println("id: ${it.id} ${it.words}") }
+                        println("test-$testId time cost: ${timeCost}ms latency: ${reply.latency}us, time: ${reply.timeUs}us")
+                    }
+
+                }
+
+                override fun onError() {
+                    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+                }
+
+            })
+        }
+
+        private fun testProtoMsg(msg: CodeInProto.LoginReply) {
+            val msgStr = msg.toString()
+            println("reply: $msgStr")
+            var b = msg.toBuilder()
+            b.userInfoBuilder.name = "John Great"
+            var newMsg = b.build()
+            val str1 = msg.toString()
+            val str2 = newMsg.toString()
+            println("orig msg: $str1, new msg: $str2")
+        }
+
+        private fun testPb() {
+             var http = Http()
             //Http().get("http://gw.codein.net/protocol.html")
             http.post("https://gw.codein.net/echo", "{\"hello world\"}")
             //http.post("test", 0, Reply())
             //http.get("http://gw.codein.net/protocol.html")
             //http.post("https://gw.codein.net/echo", "{\"hello world\"}")
             var builder = CodeInProto.LoginRequest.newBuilder()
+            builder.phone = "18011112222"
+            builder.passwd = "EatChickenTonight"
             var req = builder.build()
-            http.post("hello", req, builder)
+            var repBuilder = CodeInProto.LoginReply.newBuilder()
+            http.post(req, repBuilder)
+            var msg = repBuilder.build()
+
+            testProtoMsg(msg)
+        }
+
+        @Throws(Exception::class)
+        @JvmStatic
+        fun main(args: Array<String>) {
+            (0 until 100).forEach {
+                testGw(it)
+            }
         }
     }
 }
--- src/main/kotlin/hello/Mqtt.kt
+++ src/main/kotlin/hello/Mqtt.kt
@@ -1,8 +1,228 @@
 package hello
+
+import java.util.*
+import kotlin.experimental.and
+import kotlin.concurrent.thread
 import org.eclipse.paho.client.mqttv3.*
 import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence
+import java.sql.Timestamp
+
+import com.google.protobuf.Message
+import protofiles.protojava.CommonProtos as CommonProto
+import protofiles.protojava.MessagingProto
+import protofiles.protojava.UserProto
+import protofiles.protojava.CodeInProtos
+import protofiles.protojava.MessagingProto.LiveMessageType
+import protofiles.protojava.MessagingProto.InstantMessageType
+
 //import org.eclipse.paho.mqttv5.client.*
 //import org.eclipse.paho.mqttv5.client.persist.MemoryPersistence
 
-class Mqtt {
+interface MqttListener {
+    fun onGameContactInfo(msg: MessagingProto.GameContactInfo, uid: Long, mid: Long) {}
+}
+
+class Mqtt : MqttCallbackExtended {
+    private var listener = object: MqttListener{
+        // null
+    }
+
+    private var brokerUrl: String? = null
+    private var client: MqttClient? = null
+
+    override fun connectComplete(reconnect: Boolean, serverURI: String) {
+        val time = Timestamp(System.currentTimeMillis())
+        for (topic in topics_) {
+            subscribe(topic, qos_)
+        }
+    }
+
+    override fun connectionLost(cause: Throwable?) {
+        val time = Timestamp(System.currentTimeMillis())
+        Thread.sleep(1000L)
+        try {
+            connect(broker_, clientId_, true, userName_, password_)
+            for (topic in topics_) {
+                subscribe(topic, qos_)
+            }
+        } catch (e: MqttException) {
+            println("reason " + e.reasonCode)
+            println("msg " + e.message)
+            println("loc " + e.localizedMessage)
+            println("cause " + e.cause)
+            println("excep " + e)
+            e.printStackTrace()
+        }
+    }
+
+    override fun messageArrived(topic: String?, msg: MqttMessage?) {
+        val time = Timestamp(System.currentTimeMillis())
+
+        if (msg?.payload == null || topic == null || msg.payload.size < 10) {
+            return
+        }
+        try {
+            var uid = 0L
+            when {
+                topic.startsWith("codein/live/") -> {
+                    topic.substring(12).toLong()
+                }
+                topic.startsWith("im/user/") -> return
+                topic.startsWith("im/group/") -> return
+                else -> return
+            }
+
+            val arr = msg.payload as ByteArray
+            val type = (arr[0] and 0xff.toByte()).toInt() shl 8 or
+                    (arr[1] and 0xff.toByte()).toInt()
+            val id = (arr[0] and 0xff.toByte()).toLong() shl 56 or
+                    ((arr[3] and 0xff.toByte()).toLong() shl 48) or
+                    ((arr[4] and 0xff.toByte()).toLong() shl 40) or
+                    ((arr[5] and 0xff.toByte()).toLong() shl 32) or
+                    ((arr[6] and 0xff.toByte()).toLong() shl 24) or
+                    ((arr[7] and 0xff.toByte()).toLong() shl 16) or
+                    ((arr[8] and 0xff.toByte()).toLong() shl 8) or
+                    (arr[9] and 0xff.toByte()).toLong()
+
+            //when (InstantMessageType.forNumber(type)) {
+            //    InstantMessageType.IM_CHAT_MESSAGE -> {
+            //        var req = MessagingProto.ImChatMsg.parseFrom(arr.sliceArray(10 until arr.size));
+            //        val str = req.toString()
+            //        onImChatMsg(req, topic, id);
+            //    }
+            //    else -> {
+            //    }
+            //}
+            when (LiveMessageType.forNumber(type)) {
+                LiveMessageType.LMT_GAME_CONTACT_INFO -> {
+                    var msg = MessagingProto.GameContactInfo.parseFrom(arr.sliceArray(10 until arr.size));
+                    listener.onGameContactInfo(msg, uid, id)
+                }
+                else -> {
+                }
+            }
+        } catch (e: com.google.protobuf.InvalidProtocolBufferException) {
+        } catch (e: Exception) {
+
+        }
+
+        //println("Msg Arrive Time: $time Topic: $topic Qos: ${msg?.qos}\nPayload: $str")
+    }
+
+    override fun deliveryComplete(token: IMqttDeliveryToken?) {
+        //LOGGER.info("message delivery complete. token $token")
+    }
+
+
+    fun connect(brokerUrl: String, clientId: String, cleanSession: Boolean, userName: String?, password: String?): Boolean {
+        this.brokerUrl = brokerUrl
+        val persistence = MemoryPersistence()
+        var ok = false
+        try {
+            val connOpt = MqttConnectOptions()
+            connOpt.isAutomaticReconnect = true
+            connOpt.isCleanSession = cleanSession
+            if (userName != null) {
+                connOpt.userName = userName;
+                if (password != null)
+                    connOpt.password = password.toCharArray()
+            }
+
+
+            client = MqttClient(brokerUrl, clientId, persistence)
+            client?.setCallback(this)
+            client?.connect(connOpt)
+            ok = true
+        } catch (e: MqttException) {
+            e.printStackTrace()
+            println("error connect to $brokerUrl $e")
+        }
+
+        return ok;
+    }
+
+    fun publish(topic: String, qos: Int, payload: ByteArray){
+        val time = Timestamp(System.currentTimeMillis())
+        println("topic: $topic qos: $qos payload: ${payload.toString()}")
+        val msg = MqttMessage()
+        msg.payload = payload
+        msg.qos = qos
+        client?.publish(topic, msg)
+    }
+
+    fun subscribe(topic: String, qos: Int) {
+        println("subscribing to topic $topic qos $qos")
+        client?.subscribe(topic, qos)
+    }
+
+    var qos_ = 1
+    var topics_ = arrayOf("im/sys/#")
+    var broker_ = "tcp://localhost:1883"
+    var clientId_ = "kt_let_" + (Math.random() * 1e12).toLong().toString(16)
+    var userName_ =  "codein_os_kt"
+    var password_ = "os.cOdein.tv"
+
+    init {
+        if (clientId_.length < 1)
+            clientId_ = "kt_let_" + (Math.random() * 1e8).toLong().toString()
+    }
+
+    fun start() {
+        try {
+            if (connect(broker_, clientId_, true, userName_, password_)) {
+            }
+        } catch (e: MqttException) {
+            println("reason " + e.reasonCode)
+            println("msg " + e.message)
+            println("loc " + e.localizedMessage)
+            println("cause " + e.cause)
+            println("excep " + e)
+            e.printStackTrace()
+        }
+    }
+
+    companion object {
+        private fun testMqtt(testId: Int) {
+            val req = CodeInProtos.GetMwRequest.newBuilder()
+            req.gameId = 0
+            val clinfo = req.clientInfoBuilder
+            clinfo.uid = 2017009L
+            clinfo.token = "kt_coro_http"
+            clinfo.deviceId = "kt_coro_dev_000"
+            val reply = CodeInProtos.GetMwReply.newBuilder()
+            val start = System.currentTimeMillis()
+            Http().gwCall("GetMw", req.build(), reply, object: GwCallback {
+                override fun onReply(reply: Message) {
+                    if (reply is CodeInProtos.GetMwReply) {
+                        val timeCost = System.currentTimeMillis() - start
+                        (0 until reply.listCount)
+                                .map { reply.getList(it) }
+                                .forEach { println("id: ${it.id} ${it.words}") }
+                        println("test-$testId time cost: ${timeCost}ms latency: ${reply.latency}us, time: ${reply.timeUs}us")
+                    }
+
+                }
+
+                override fun onError() {
+                    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+                }
+
+            })
+            val topic = "codein/live/a"
+            try {
+                val uid = topic.substring(12).toLong()
+                println("uid = $uid")
+            } catch(e: Exception) {
+                e.printStackTrace()
+            }
+        }
+
+        @Throws(Exception::class)
+        @JvmStatic
+        fun main(args: Array<String>) {
+            (0 until 1).forEach {
+                testMqtt(it)
+            }
+        }
+    }
 }
